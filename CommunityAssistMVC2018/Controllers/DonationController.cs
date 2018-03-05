e471743db35dc46f22cb68f40bfc4492e1206005
griffin dotson <griffin_dotson@yahoo.com>
2018-03-05 11:24:52 -0800

    Donate

---
+++ CommunityAssistMVC2018/Controllers/DonationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistMVC2018.Models;
+
+namespace CommunityAssistMVC2018.Controllers
+{
+    public class DonationController : Controller
+    {
+        // GET: Donation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index([Bind(Include = "Donation")]DonationClass donate)
+        {
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+            if (Session["PersonKey"] != null)
+            {
+                Donation d = new Donation();
+                d.PersonKey = (int) Session["PersonKey"];
+                d.DonationDate = DateTime.Now;
+                d.DonationAmount = donate.Donation;
+       
+                d.DonationConfirmationCode = Guid.NewGuid();
+                db.Donations.Add(d);
+                db.SaveChanges();
+            }
+
+            return View("Result");
+        }
+    }
+}
---
+++ CommunityAssistMVC2018/Models/DonationClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssistMVC2018.Models
+{
+    public class DonationClass
+    {
+        public decimal Donation { get; set; }
+        public string PersonKey { get; set; }
+    }
+}
